cd7aa0c9870c90d5c1600a3f54bc30d761c8082f
c32tao@uwaterloo.ca <c32tao@uwaterloo.ca>
2023-09-16 09:52:11 -0400

    i c

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from "react"
 
 function App() {
+  const [userName, setUserName] = useState("")
+  const [password, setPassword] = useState("")
+
+  //userName = "jenny" //setUser("jenny")
+
+  const saveData = async (e) => {
+    e.preventDefault();
+      const res = await fetch("localhost:3000/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          userName,
+          password
+        }),
+      });
+
+  }
+
+  // const handleSubmit = async (e) => {
+  //   const res = await fetch("https://giftrackerapi.onrender.com/signup/", {
+  //     method: "POST",
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //     },
+  //     body: JSON.stringify({
+  //       email,
+  //       password,
+  //     }),
+  //   });
+
+  //   const data = await res.json();
+
+  //   console.log("res", res);
+
+  //   console.log("data", data);
+
+  //   if (data.userCreated) {
+  //     setGoToLogin(true);
+  //   } else {
+  //     setErrorMessage(data.message);
+  //   }
+  // };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>FORM</h1>
+      <form onSubmit={saveData}>
+        <h4>Username</h4>
+        <input value={userName} onChange={(e) => {
+          setUserName(e.target.value)
+          console.log("username", userName)
+        }}>
+        </input>
+        <h4>Password</h4>
+        <input value={password} onChange={(e) => {
+          setPassword(e.target.value)
+          console.log("password", password)
+        }}>
+        </input>
+        <button>Submit</button>
+      </form>
     </div>
   );
 }

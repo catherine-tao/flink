34ec37e2d82c651636a1a8b94685a13de1dc53b5
j3nny-zhang <jennyzhanng@gmail.com>
2023-09-16 17:38:19 -0400

    openai image generation

---
+++ src/pages/background.js
@@ -0,0 +1,50 @@
+// secret key: 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
+import { useState } from "react";
+
+function Background() {
+    const [backgroundUrl, setBackgroundUrl] = useState("");
+
+    const generate = async(e) => {
+        e.preventDefault();
+
+        console.log("reacched");
+        
+        const OpenAI = require("openai");
+        const openai = new OpenAI({
+            apiKey: "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",
+            dangerouslyAllowBrowser: true
+        });
+
+        const prompt = "generate a background image for makeup, beauty, vlogger";
+
+        const result = await openai.images.generate
+        ({
+            prompt: prompt,
+            n: 1,
+            size: "1024x1024",
+        })
+
+        setBackgroundUrl(result.data[0].url);
+        console.log(backgroundUrl); 
+        console.log(result.data[0].url); 
+    
+        // const res = await fetch("http://localhost:3000/updateData", {
+        //     method: "PUT",
+        //     headers: {
+        //     "Content-Type": "application/json",
+        //     },
+        //     body: JSON.stringify({
+        //         backgroundUrl,
+        //     }),
+        // });
+
+    }
+
+    return (
+      <div>
+        <button onClick={generate}>generate image</button>
+      </div>
+    );
+  }
+  
+  export default Background;

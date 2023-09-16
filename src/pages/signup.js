8fd71111c745fac7ba8e27c9327a6d60f36eb391
mayxly <may.ly7303@gmail.com>
2023-09-16 11:40:22 -0400

    CSS global styles + sign up page setup

---
+++ src/pages/signup.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function SignUp({ show }) {
+  return (
+    <div className="background-green vh-100">
+      <div className="white-right-panel"></div>
+    </div>
+  );
+}

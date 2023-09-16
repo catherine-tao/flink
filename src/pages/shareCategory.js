c901b869a16fc8b275b9c142333e1211ffb2e5e5
Joyce Ye <yejoyce8@gmail.com>
2023-09-16 12:40:45 -0400

    add font to css

---
+++ src/pages/shareCategory.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function ShareCategory({ show }) {
+  return (
+    <div className="background-green vh-100">
+      <div className="white-right-panel">
+        <h1>Share a bit about yourself</h1>
+      </div>
+    </div>
+  );
+}

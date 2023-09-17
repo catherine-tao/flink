11befb8f0b80be7fa1251014f35c19b3fe8fa429
Joyce Ye <yejoyce8@gmail.com>
2023-09-16 21:16:43 -0400

    background generation

--- src/pages/describeYourself.js
+++ src/pages/describeYourself.js
@@ -3,12 +3,62 @@ import React, {useState} from "react";
 import Flowers from "../assets/Flowers.png"
 import Logo from "../assets/Logo.svg"
 import Form from 'react-bootstrap/Form';
+import LoadingBackground from "./loadingBackground";
 import { Link } from "react-router-dom";
 
 
-export default function DescribeYourself({ show }) {
-    const [isLoading, setIsLoading] = useState(false)
+
+
+export default function DescribeYourself({ email}) {
+    const [isLoading, setIsLoading] = useState(false);
+    const [isFilled, setIsFIlled] = useState(false);
+    
+    const [allowProceed, setAllowProceed] = useState(false);
+    const [backgroundUrl, setBackgroundUrl] = useState("");
+
+    const [word1, setword1] = useState("");
+    const [word2, setword2] = useState("");
+    const [word3, setword3] = useState("");
+ 
+
+    const generate = async(e) => {
+        e.preventDefault();
+
+        console.log("reacched");
+        
+        const OpenAI = require("openai");
+        const openai = new OpenAI({
+            apiKey: process.env.API_KEY,
+            dangerouslyAllowBrowser: true
+        });
+
+        const prompt = `generate a background image for ${word1}, ${word2}, ${word3}`;
+
+        const result = await openai.images.generate
+        ({
+            prompt: prompt,
+            n: 1,
+            size: "1024x1024",
+        })
+
+        setBackgroundUrl(result.data[0].url);
+        console.log(backgroundUrl); 
+        console.log(result.data[0].url); 
+    
+        // const res = await fetch(`http://localhost:3000/updateData/${{email}}`, {
+        //     method: "POST",
+        //     headers: {
+        //     "Content-Type": "application/json",
+        //     },
+        //     body: JSON.stringify({
+        //         backgroundUrl,
+        //     }),
+        // } );
+       }
+
     return (
+      <div>
+        {!allowProceed && (
     <div className="background-green vh-100">
       <h2 className="green-left-panel-text">Flink helps generate personal landing pages to make your content appealing.</h2>
       <img className="flower-image-panel" src={Flowers}/>
@@ -24,23 +74,40 @@ export default function DescribeYourself({ show }) {
           <Form>
             <Form.Group className="mb-3">
                 <Form.Label>1</Form.Label>
-                <Form.Control placeholder="Your first word"/>
+                <Form.Control placeholder="Your first word"
+                value={word1}
+                onChange={(e) => {
+                  setword1(e.target.value);
+                  console.log("word1", word1);
+                }}/>
             </Form.Group>
             <Form.Group className="mb-3">
                 <Form.Label>2</Form.Label>
-                <Form.Control placeholder="Your second word" />
+                <Form.Control placeholder="Your second word"
+                 value={word2}
+                 onChange={(e) => {
+                   setword2(e.target.value);
+                   console.log("word2", word2);
+                 }} />
             </Form.Group>
             <Form.Group className="mb-3">
                 <Form.Label>3</Form.Label>
-                <Form.Control placeholder="Your third word" />
+                <Form.Control placeholder="Your third word" 
+                 value={word3}
+                 onChange={(e) => {
+                   setword3(e.target.value);
+                   console.log("word3", word3);
+                 }}/>
             </Form.Group>
-            <button className="green-button mt-5" onClick={() => {setIsLoading(true)}}>
-              Generate
+            <button className="green-button mt-5" onClick={generate}>
+              Continue
             </button>
           </Form>
         </div> 
         }
       </div>
-    </div>
+    </div>) }
+    {allowProceed && <LoadingBackground email={email} backgroundUrl={backgroundUrl} />}
+      </div>
   );
 }
